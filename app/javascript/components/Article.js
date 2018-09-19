414b7e1bed4affcd46832e99946877d24ef38056
kagose yusuke <yusukekagose@icloud.com>
2018-09-19 17:30:56 +0900

    completed change article to react component and added react timestamp

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,14 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import Timestamp from "react-timestamp"
+
 class Article extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <div class="article-title">
+        <div className="article-title">
           <a href={this.props.path}>{this.props.title}</a>
         </div>
-        <div class="article-body">
-        {this.props.description}
+        <div className="article-body">
+          {this.props.description}
+          <div className="article-meta-details">
+            <small>Created by: {this.props.author},
+                               &nbsp;
+                               <Timestamp time={this.props.created_at} precision={3} />,
+                               last updated: <Timestamp time={this.props.updated_at} precision={3} />
+                               </small>
+          </div>
         </div>
       </React.Fragment>
     );
@@ -19,5 +29,8 @@ Article.propTypes = {
   title: PropTypes.string,
   path: PropTypes.string,
   description: PropTypes.string,
+  author: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string,
 };
 export default Article
